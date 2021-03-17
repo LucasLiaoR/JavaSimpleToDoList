90fee866b19e997a2bb0b9a4cad9b54523ef4a99
LucasLiaoR <lucasliao6@gmail.com>
2021-03-17 14:58:41 +0100

    Bouton ajout de tâche +bouton supprimer tâches finies

--- src/InterfaceUtilisateur.java
+++ src/InterfaceUtilisateur.java
@@ -2,10 +2,13 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import java.awt.FlowLayout;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
@@ -16,6 +19,7 @@ import javax.swing.JTextField;
 
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
@@ -26,6 +30,7 @@ import javax.swing.GroupLayout.Alignment;
 public class InterfaceUtilisateur {
 
 	private JFrame frame;
+	private JTextField textFieldAddTask;
 
 	/**
 	 * Launch the application.
@@ -55,28 +60,70 @@ public class InterfaceUtilisateur {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 507, 348);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+		frame.setLocationRelativeTo(null);
+		frame.setTitle("To Do List");
 		JPanel panel = new JPanel();
-		panel.setBounds(80, 0, 289, 218);
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		//panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		panel.setBounds(54, 0, 387, 266);
 		frame.getContentPane().add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
-		JButton btnNewButton = new JButton("Ajouter une t\u00E2che");
-		btnNewButton.setMargin(new Insets(0, 0, 0, 0));
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btnAddTask = new JButton("Ajouter une t\u00E2che");
+		btnAddTask.setMargin(new Insets(0, 0, 0, 0));
+		btnAddTask.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JTextField newArea = new JTextField("test",0);
-				newArea.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
-				panel.add(newArea);
-				frame.revalidate();
+				
+				if (!textFieldAddTask.getText().equals(""))
+				{
+					JCheckBox c1 = new JCheckBox(textFieldAddTask.getText());
+					panel.add(c1);
+					textFieldAddTask.setText("");
+					frame.revalidate();
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Vous n'avez pas saisi de tâche à accomplir !");
+				}
+				
 			}
 		});
-		btnNewButton.setBounds(169, 238, 117, 23);
-		frame.getContentPane().add(btnNewButton);
+		btnAddTask.setBounds(221, 277, 108, 23);
+		frame.getContentPane().add(btnAddTask);
+		
+		textFieldAddTask = new JTextField();
+		textFieldAddTask.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldAddTask.setBounds(10, 278, 189, 20);
+		frame.getContentPane().add(textFieldAddTask);
+		textFieldAddTask.setColumns(10);
+		
+		JButton btnClear = new JButton("Retirer t\u00E2ches finies");
+		btnClear.addActionListener(new ActionListener() {
+			@SuppressWarnings("deprecation")
+			public void actionPerformed(ActionEvent arg0) {
+				for (Component panelComponent : panel.getComponents())
+				{
+					JCheckBox checkBox = (JCheckBox)panelComponent;
+					
+					if (checkBox.isSelected())
+					{
+						System.out.println(checkBox.getLabel());
+						panel.remove(checkBox);
+						panel.revalidate();
+						panel.repaint();
+					}
+					
+				}
+				
+				//System.out.println("Tester");
+			}
+		});
+		btnClear.setMargin(new Insets(0, 0, 0, 0));
+		btnClear.setBounds(339, 277, 128, 23);
+		frame.getContentPane().add(btnClear);
+		
+		
 		
 		
 		
